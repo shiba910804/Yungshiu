7a3d9e835337e00c9cdca0d8dbf5fcd8b724617d
shiba910804 <kotori910804@gmail.com>
2026-06-12 21:35:27 +0800

    Allow dashboard host and port configuration

--- app.py
+++ app.py
@@ -377,4 +377,6 @@ def download(filename):
 
 if __name__ == "__main__":
     debug = os.environ.get("FLASK_DEBUG", "").lower() in {"1", "true", "yes"}
-    app.run(debug=debug, host="127.0.0.1", port=5000)
+    host = os.environ.get("FLASK_HOST", "0.0.0.0")
+    port = int(os.environ.get("PORT", "5000"))
+    app.run(debug=debug, host=host, port=port)
